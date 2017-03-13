3ce7f4f3853c6497e5da0991a7a2fe98395c2b0b
Terry McDonnell <terry.mcdonnell@gmail.com>
2017-03-13 15:31:22 -0700

    minor updates and integration

--- auction/auction/admin.py
+++ auction/auction/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.db import models
+from django.forms import Textarea
 import nested_admin
 from .models import Lot, Wine, Item
 
@@ -17,5 +19,44 @@ class LotAdmin(nested_admin.NestedModelAdmin):
     inlines = [
         ItemInline,
     ]
+    formfield_overrides = {
+        models.TextField: {'widget': Textarea(attrs={'cols': 80})},
+    }
+    fieldsets = (
+        ('Main', {
+            'fields': ('type', 'lot', 'title',)
+        }),
+        ('Description', {
+            'fields': ('short_desc', 'description', 'restrictions', 'image')
+        }),
+        ('Advanced', {
+            'classes': ('collapse', ),
+            'fields': ('item_description_override',)
+        }),
+        ('Financial', {
+            'fields': ('FMV', 'predicted_sale', 'cost', 'start_bid', 'min_raise',)
+        }),
+        ('Review', {
+            'fields': ('reviewed', 'received', 'confirmed',)
+        }),
+        ('Admin', {
+            'fields': ('notes',)
+        }),
+    )
+    list_filter = ('type', )
+    search_fields = ['title', 'short_desc']
+
+class WineAdmin(admin.ModelAdmin):
+    list_display = ('item', 'year', 'description', 'size', 'qty', 'rating', 'confirmed')
+    search_fields = ('year', 'description')
+    list_editable = ('year', 'description', 'size', 'qty', 'rating', 'confirmed')
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('lot', 'description', 'onsite_pickup', 'contact_name', 'contact_point', 'confirmed')
+    search_fields = ('lot__title', 'description')
+    list_editable = ('description', 'onsite_pickup', 'contact_name', 'contact_point', 'confirmed')
 
 admin.site.register(Lot, LotAdmin)
+admin.site.register(Wine, WineAdmin)
+admin.site.register(Item, ItemAdmin)
+
--- auction/auction/models.py
+++ auction/auction/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.safestring import mark_safe
 
 # Create your models here.
 class Lot(models.Model):
@@ -24,9 +25,13 @@ class Lot(models.Model):
     FMV = models.PositiveIntegerField(null=True)
     predicted_sale = models.PositiveIntegerField(null=True)
     cost = models.PositiveIntegerField(default=0)
-    image = models.ImageField(upload_to='uploads/', null=True)
+    image = models.ImageField(upload_to='uploads/', null=True, blank=True)
     start_bid = models.PositiveIntegerField(null=True)
     min_raise = models.PositiveIntegerField(null=True)
+    complete = models.BooleanField(
+        default=False,
+        help_text='The lot description and items are complete based on donation form'
+    )
     reviewed = models.BooleanField(
         default=False,
         help_text='The lot has been reviewed for accuracy by an auction chair'
@@ -39,26 +44,40 @@ class Lot(models.Model):
         default=False,
         help_text='The items has been reviewed complete and available on-site for sale'
     )
+    notes = models.TextField(
+        blank=True,
+        help_text='Internal notes on the lot'
+    )
+    item_description_override = models.TextField(
+        blank=True,
+        help_text='A text description of the items to override the default'
+    )
+    def image_thumbnail(self):
+        return mark_safe('<img src="/uploads/%s" width="50" height="5-" />' % (self.image))
+    image_thumbnail.short_description = 'Preview'
+    image_thumbnail.allow_tags = True
     def __str__(self):
         return self.type + '-' + str(self.lot) + '-' + self.title
 
 class Item(models.Model):
-    lot = models.ForeignKey(Lot, on_delete=models.CASCADE)
+    lot = models.ForeignKey(Lot, on_delete=models.CASCADE, null=True)
     description = models.CharField(max_length=140)
     onsite_pickup = models.BooleanField(default=True)
     contact_name = models.CharField(max_length=50)
     contact_point = models.CharField(max_length=50)
+    confirmed = models.BooleanField(default=False)
     def __str__(self):
         return self.description
 
 
 class Wine(models.Model):
-    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, null=True)
     year = models.PositiveSmallIntegerField()
     description = models.CharField(max_length=50)
     size = models.CharField(default='750 ml', max_length=10)
     qty = models.PositiveSmallIntegerField(default=1)
     rating = models.CharField(blank=True, max_length=10)
+    confirmed = models.BooleanField(default=False)
     def __str__(self):
         return str(self.year) + ' ' + self.description + ' (' + self.size + ') x' + str(self.qty)
     @property

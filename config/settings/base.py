54e1efdeda76552b0e81cf9abe47802ab431db47
tmcdonnell87 <terry.mcdonnell@gmail.com>
2017-04-05 09:51:04 -0700

    live slide

--- config/settings/base.py
+++ config/settings/base.py
@@ -92,7 +92,7 @@ MIGRATION_MODULES = {
 # ------------------------------------------------------------------------------
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#debug
 DEBUG = env.bool('DJANGO_DEBUG', False)
-
+WKHTMLTOPDF_DEBUG = DEBUG
 # FIXTURE CONFIGURATION
 # ------------------------------------------------------------------------------
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#std:setting-FIXTURE_DIRS
